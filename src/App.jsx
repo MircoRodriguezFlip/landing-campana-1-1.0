e46d63b51d4e2e070d3de487c283bac16ba40b17
mircomania <mirco.rodriguez@hotmail.com>
2025-07-07 15:35:06 -0400

    6 commit landing-campaña - cambio de color fondo, textos y se agrega logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { ScrollToTop } from './hooks/ScrollTop';
+import { NavBar } from './components/common/Navbar';
 import { LandingPage } from './components/pages/LandingPage';
 
 function App() {
     return (
         <BrowserRouter>
             <ScrollToTop />
+
+            <NavBar />
 
             <Routes>
                 <Route path="/" element={<LandingPage />} />
